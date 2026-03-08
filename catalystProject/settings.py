6a54dd846784454fde59d9f91cd16b4c8c091227
malu123-cpu <malumr2006@gmail.com>
2026-03-08 10:58:38 +0530

    Fix deployment: added requirements.txt and configured WhiteNoise

--- catalystProject/settings.py
+++ catalystProject/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,6 +135,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT=os.path.join(BASE_DIR,'images')
 MEDIA_URL = '/images/'
 
